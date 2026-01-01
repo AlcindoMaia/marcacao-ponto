c9f571da7eab88408fe5adc1d98158d662498afd
AlcindoMaia <eng.alcindo.maia@gmail.com>
2026-01-01 18:58:52 +0000

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,10 +1,10 @@
 // =======================================================
 // SUPABASE (CHAVE CORRETA)
 // =======================================================
-const SB = supabase.createClient(
-  "https://npyosbigynxmxdakcymg.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5weW9zYmlneW54bXhkYWtjeW1nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4MzYyMjYsImV4cCI6MjA4MDQxMjIyNn0.CErd5a_-9HS4qPB99SFyO-airsNnS3b8dvWWrSPE4_M"
-);
+const SUPABASE_URL = "https://npyosbigynxmxdakcymg.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5weW9zYmlneW54bXhkYWtjeW1nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4MzYyMjYsImV4cCI6MjA4MDQxMjIyNn0.CErd5a_-9HS4qPB99SFyO-airsNnS3b8dvWWrSPE4_M";
+
+const SB = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 const PIN_ADMIN = "1810";
 
