8424e01f0f873dea0a53d196d313af0d56e81b0b
AlcindoMaia <eng.alcindo.maia@gmail.com>
2026-03-04 13:43:16 +0000

    Change Supabase anonymous key in admin.js
    Updated the Supabase anonymous key for the admin module.

--- admin.js
+++ admin.js
@@ -3,7 +3,7 @@
 // =======================================================
 const SUPABASE_URL = "https://npyosbigynxmxdakcymg.supabase.co";
 const SUPABASE_ANON_KEY =
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5weW9zYmlneW54bXhkYWtjeW1nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4MzYyMjYsImV4cCI6MjA4MDQxMjIyNn0.CErd5a_-9HS4qPB99SFyO-airsNnS3b8dvWWrSPE4_M";
 
 const SB = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 const PIN_ADMIN = "1810";
